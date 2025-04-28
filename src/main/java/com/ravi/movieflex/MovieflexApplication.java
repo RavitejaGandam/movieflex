e06d4cbfb2e0e539aab3ab14177c218aa90405a2
Raviteja Gandam <gandamraviteja@gmail.com>
2025-04-28 11:00:22 +0530

    Initial setup

---
+++ src/main/java/com/ravi/movieflex/MovieflexApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.movieflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieflexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieflexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravi/movieflex/MovieflexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.movieflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieflexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
