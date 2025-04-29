4cdfac3122d56c59da1fa6c3d980850af75f870e
Raviteja Gandam <gandamraviteja@gmail.com>
2025-04-29 10:42:45 +0530

    File uploading downloading completed

--- src/main/java/com/ravi/movieflex/MovieflexApplication.java
+++ src/main/java/com/ravi/movieflex/MovieflexApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MovieflexApplication {
 
+	//50:39
 	public static void main(String[] args) {
 		SpringApplication.run(MovieflexApplication.class, args);
 	}
